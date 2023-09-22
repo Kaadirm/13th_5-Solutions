d558ff5fa4c11cf50ec799775ba58bff498efd83
Kadir <abldkrmmsglu@gmail.com>
2023-09-22 12:36:43 +0300

    file added

---
+++ 13th_5-Solutions.js
@@ -0,0 +1,177 @@
+/*
+------   1   -------
+
+Sorted Union
+Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
+
+In other words, all values present from all arrays should be included in their original order, but with no duplicates in the final array.
+
+The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.
+
+Check the assertion tests for examples.
+
+
+
+SOLUTION
+
+function uniteUnique(...arr) {
+    const totalArr = []
+    for(let i = 0; i < arr.length; i++) {
+        for(let j = 0; j < arr[i].length; j++) {
+            if(totalArr.includes(arr[i][j]) === false){
+                totalArr.push(arr[i][j])
+            }
+        }
+    }
+    return totalArr
+}
+ 
+
+
+------   2   -------
+
+Palindrome Checker
+
+Return true if the given string is a palindrome. Otherwise, return false.
+
+A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
+
+Note: You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn everything into the same case (lower or upper case) in order to check for palindromes.
+
+We'll pass strings with varying formats, such as racecar, RaceCar, and race CAR among others.
+
+We'll also pass strings with special symbols, such as 2A3*3a2, 2A3 3a2, and 2_A3*3#A2.
+
+
+SOLUTION
+
+
+function palindrome(str) {
+    let myVar = str.match(/[A-Za-z0-9]+/gi).join("").toLowerCase().split("");
+    if (myVar.join("") === myVar.reverse().join("")){
+        return true
+    }
+    return false
+}
+
+
+------   3   -------
+
+Roman Numeral Converter
+Convert the given number into a roman numeral.
+
+Roman numerals	Arabic numerals
+M	1000
+CM	900
+D	500
+CD	400
+C	100
+XC	90
+L	50
+XL	40
+X	10
+IX	9
+V	5
+IV	4
+I	1
+All roman numerals answers should be provided in upper-case.
+
+
+SOLUTION
+
+function convertToRoman(num) {
+ const table = {   
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    III: 3,
+    II: 2,
+    I: 1 
+  }
+  let accumulator = "";
+
+  for (const key in table){
+    const numberValue = table[key];
+    while(numberValue <= num){
+      num -= numberValue
+      accumulator += key;
+    }
+  }
+  return accumulator
+}
+
+------   4   -------
+
+Caesars Cipher
+One of the simplest and most widely known ciphers is a Caesar cipher, also known as a shift cipher. In a shift cipher the meanings of the letters are shifted by some set amount.
+
+A common modern use is the ROT13 cipher, where the values of the letters are shifted by 13 places. Thus A ↔ N, B ↔ O and so on.
+
+Write a function which takes a ROT13 encoded string as input and returns a decoded string.
+
+All letters will be uppercase. Do not transform any non-alphabetic character (i.e. spaces, punctuation), but do pass them on.
+
+
+SOLUTION 
+
+const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+  
+
+function rot13(str) {
+  let accumulator = "";
+  for(let i = 0; i<str.length; i++){
+    const char = str[i];
+    const isALetter = alphabet.includes(char);
+    if(isALetter === false){
+      accumulator += char;
+    }
+    else{
+      const charIndex = alphabet.findIndex((c) => c === char);
+
+      accumulator += alphabet[charIndex + 13] || alphabet[charIndex - 13]
+    }
+  }
+  return accumulator
+}
+
+
+------   5   -------
+
+Telephone Number Validator
+Return true if the passed string looks like a valid US phone number.
+
+The user may fill out the form field any way they choose as long as it has the format of a valid US number. 
+
+The following are examples of valid formats for US numbers (refer to the tests below for other variants):
+
+555-555-5555
+(555)555-5555
+(555) 555-5555
+555 555 5555
+5555555555
+1 555 555 5555
+
+For this challenge you will be presented with a string such as 800-692-7753 or 8oo-six427676;laskdjf. 
+
+Your job is to validate or reject the US phone number based on any combination of the formats provided above. The area code is required. 
+
+If the country code is provided, you must confirm that the country code is 1. Return true if the string is a valid US phone number; otherwise return false.
+
+
+SOLUTION
+
+function telephoneCheck(str) {
+  const reg = /^(1\s|1)?(\(\d{3}\)|\d{3})(-|\s)?\d{3}(-|\s)?\d{4}$/
+  return reg.test(str)
+}
+
+*/
